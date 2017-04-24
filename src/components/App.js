218988fa0535a8ee4cd718d451b2947169fa9024
christopher Thronebury <Chris@christophers-MacBook.local>
2017-04-23 20:21:48 -0400

    Create Fish Component and Display

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+
 
         this.state = {
             fishes: {},
@@ -27,14 +31,28 @@ class App extends React.Component {
         //save our state
         this.setState({ fishes });
     }
+
+    loadSamples(){
+        this.setState({
+           fishes: sampleFishes
+        });
+    }
+
     render () {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="list-of-fishes">
+                        {
+                            Object
+                                .keys(this.state.fishes)
+                                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
         )
     }
